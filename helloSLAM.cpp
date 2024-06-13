e3fc204c77254e842cee0e0532807399d55d56b0
Xiawenlong-bug <2473833028@qq.com>
2024-06-13 12:03:23 +0800

    first commit for helloSLAM

---
+++ helloSLAM.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: Xiawenlong-bug 2473833028@qq.com
+ * @Date: 2024-06-13 11:46:15
+ * @LastEditors: Xiawenlong-bug 2473833028@qq.com
+ * @LastEditTime: 2024-06-13 11:47:35
+ * @FilePath: /VslamPractice/helloSLAM.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "hello" << endl;
+    return 0;
+}
