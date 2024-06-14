b5793f861455e592c090c258fd47489f309c246d
Xiawenlong-bug <2473833028@qq.com>
2024-06-14 20:31:23 +0800

    采用对象的方法在CMAKELISTS中加入了CERES库,下一步是用CERES库写曲线拟合

---
+++ optimization/curveFittingCeres.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: Xiawenlong-bug 2473833028@qq.com
+ * @Date: 2024-06-14 20:17:56
+ * @LastEditors: Xiawenlong-bug 2473833028@qq.com
+ * @LastEditTime: 2024-06-14 20:24:58
+ * @FilePath: /VslamPractice/optimization/curveFittingCeres.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include<iostream>
+#include<vector>
+#include<chrono>
+#include<ceres/ceres.h>
+
+
+using namespace std;
+
+int main(int argc ,char** argv)
+{
+    
+}
